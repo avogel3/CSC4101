f795a7290c3852041e5ee02f2fcaeb5e12f40748
Andrew Vogel <andrewpvogel@gmail.com>
2019-07-11 11:59:28 -0400

    Format Main with google-java-formatter

--- prog1/Main.java
+++ prog1/Main.java
@@ -1,58 +1,55 @@
 // Main.java -- the main program
 
 public class Main {
-    // Array of token names used for debugging the scanner
-    public static final String TokenName[] = {
-            "QUOTE",            // '
-            "LPAREN",           // (
-            "RPAREN",           // )
-            "DOT",              // .
-            "TRUE",             // #t
-            "FALSE",            // #f
-            "INT",              // integer constant
-            "STRING",           // string constant
-            "IDENT"             // identifier
-    };
+  // Array of token names used for debugging the scanner
+  public static final String TokenName[] = {
+    "QUOTE", // '
+    "LPAREN", // (
+    "RPAREN", // )
+    "DOT", // .
+    "TRUE", // #t
+    "FALSE", // #f
+    "INT", // integer constant
+    "STRING", // string constant
+    "IDENT" // identifier
+  };
 
-    public static void main(String argv[]) {
-        // create scanner that reads from standard input
-        Scanner scanner = new Scanner(System.in);
+  public static void main(String argv[]) {
+    // create scanner that reads from standard input
+    Scanner scanner = new Scanner(System.in);
 
-        if (argv.length > 2) {
-            System.err.println("Usage: java Main " + "[-d]");
-            System.exit(1);
-        }
-
-        // if commandline option -d is provided, debug the scanner
-        if (argv.length == 1 && argv[0].equals("-d")) {
-            // debug scanner
-            Token tok = scanner.getNextToken();
-            while (tok != null) {
-                int tt = tok.getType();
-                System.out.print(TokenName[tt]);
-                if (tt == Token.INT)
-                    System.out.println(", intVal = " + tok.getIntVal());
-                else if (tt == Token.STRING)
-                    System.out.println(", strVal = " + tok.getStrVal());
-                else if (tt == Token.IDENT)
-                    System.out.println(", name = " + tok.getName());
-                else
-                    System.out.println();
-
-                tok = scanner.getNextToken();
-            }
-        }
-
-        // Create parser
-        Parser parser = new Parser(scanner);
-        Node root;
-
-        // Parse and pretty-print each input expression
-        root = parser.parseExp();
-        while (root != null) {
-            root.print(0);
-            root = parser.parseExp();
-        }
-        System.exit(0);
+    if (argv.length > 2) {
+      System.err.println("Usage: java Main " + "[-d]");
+      System.exit(1);
     }
+
+    // if commandline option -d is provided, debug the scanner
+    if (argv.length == 1 && argv[0].equals("-d")) {
+      // debug scanner
+      Token tok = scanner.getNextToken();
+      while (tok != null) {
+        int tt = tok.getType();
+        System.out.print(TokenName[tt]);
+        if (tt == Token.INT) System.out.println(", intVal = " + tok.getIntVal());
+        else if (tt == Token.STRING) System.out.println(", strVal = " + tok.getStrVal());
+        else if (tt == Token.IDENT) System.out.println(", name = " + tok.getName());
+        else System.out.println();
+
+        tok = scanner.getNextToken();
+      }
+    }
+
+    System.out.println("Starting parser...");
+
+    // Create parser
+    Parser parser = new Parser(scanner);
+    Node root = parser.parseExp();
+
+    // Parse and pretty-print each input expression
+    while (root != null) {
+      root.print(0);
+      root = parser.parseExp();
+    }
+    System.exit(0);
+  }
 }

168aba7f6e483db452c6ac7096f6bb4491110277
Andrew Vogel <andrewpvogel@gmail.com>
2019-07-11 12:02:36 -0400

    Add constructor for Regular
    * Format with google-java-formatter

--- prog1/Regular.java
+++ prog1/Regular.java
@@ -1,12 +1,11 @@
 import java.io.*;
 
 class Regular extends Special {
- 
-    // TODO: Add any fields needed.
 
- 
-    // TODO: Add an appropriate constructor.
+  // TODO: Add any fields needed.
 
-    void print(Node t, int n, boolean p) {
+  public Regular() {}
+
+  void print(Node t, int n, boolean p) {
     }
 }

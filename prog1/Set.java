285c0a96fe24c01c923950d42186bac136abd25f
Andrew Vogel <andrewpvogel@gmail.com>
2020-02-18 11:33:31 -0500

    Add constructor for Set

--- prog1/Set.java
+++ prog1/Set.java
@@ -4,8 +4,7 @@ class Set extends Special {
  
     // TODO: Add any fields needed.
 
- 
-    // TODO: Add an appropriate constructor.
+    public Set() {}
 
     void print(Node t, int n, boolean p) {
     }

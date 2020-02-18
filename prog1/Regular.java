4c276d973fb54e8ca4c92df7faf782b9fcd2f76e
Andrew Vogel <andrewpvogel@gmail.com>
2020-02-18 10:14:52 -0500

    Update comment in Regular

--- prog1/Regular.java
+++ prog1/Regular.java
@@ -16,7 +16,7 @@ class Regular extends Special {
     }
     t.getCar().print(0);
     // We know car is going to be the second to last char in the evaluated string
-    // for example (+ 1 2), car would be 2, cdr would be )
+    // for example (+ 1 2), car would be 2, cdr would be null
     // We need to look ahead and see if the next one is a paren
     if(!t.getCdr().isNull()) System.out.print(" ");
     t.getCdr().print(0, true);

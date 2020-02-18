67cad5a0b9a8f69fc91d3e4c040bba7ec17a46f6
Andrew Vogel <andrewpvogel@gmail.com>
2020-02-18 11:26:06 -0500

    Add Lambda Constructor

--- prog1/Lambda.java
+++ prog1/Lambda.java
@@ -4,8 +4,7 @@ class Lambda extends Special {
  
     // TODO: Add any fields needed.
 
- 
-    // TODO: Add an appropriate constructor.
+    public Lambda() {}
 
     void print(Node t, int n, boolean p) {
     }

b4e557ae25c53be076e6189aad75956048fed4dd
Andrew Vogel <andrewpvogel@gmail.com>
2020-02-18 11:38:08 -0500

    Implement print for Set

--- prog1/Set.java
+++ prog1/Set.java
@@ -7,5 +7,14 @@ class Set extends Special {
     public Set() {}
 
     void print(Node t, int n, boolean p) {
+        for(int i = 0; i < n; i++) {
+            System.out.print(" ");
+        }
+        if (!p) {
+            System.out.print("(");
+        }
+        t.getCar().print(0);
+        if(!t.getCdr().isNull()) System.out.print(" ");
+        t.getCdr().print(0, true);
     }
 }
